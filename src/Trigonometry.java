728b5975f157833da9af1bd3b2d1b2f240c79b84
Boyman10 <contact@yasi.fr>
2018-04-27 21:11:04 +0200

    Working on closest Item using Trigo functions

--- src/Trigonometry.java
+++ src/Trigonometry.java
@@ -5,17 +5,20 @@ import java.math.*;
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
+ * @link https://www.movable-type.co.uk/scripts/latlong.html
  **/
 class Trigonometry {
 
     public static void main(String args[]) {
 
 
-
+        // west East
         String LON = "40.0";
+        // South North
         String LAT = "20.20";
 
         // User position :
+        // long then lat
         Position defaultPos = new Position("0;default;here;nophone;3.879483;43.608177");
 
         // Number of items :
@@ -25,8 +28,9 @@ class Trigonometry {
         ArrayList<Position> posItems = new ArrayList<>();
 
         // ex : id;name;address;phone;long;lat
-        for (int i = 0; i < N; i++) {
-            posItems.add(new Position(defibList[i]));
+        for (int i = 0; i < defibList.length; i++) {
+            if (defibList[i] != null)
+             posItems.add(new Position(defibList[i]));
         }
 
 
@@ -39,9 +43,11 @@ class Trigonometry {
         /**
          * Exemple of use DISTANCE calculation
          */
-        Position position1 = new Position("1;Maison de la Prevention Sante;6 rue Maguelone 340000 Montpellier;;3,87952263361082;43,6071285339217");
+        Position position1 = new Position(
+                "1;Maison de la Prevention Sante;6 rue Maguelone 340000 Montpellier;;3,87952263361082;43,6071285339217");
 
         System.out.println("Distance " + getDistance(defaultPos,position1));
+
     }
 
 
@@ -55,13 +61,30 @@ class Trigonometry {
 
         double distance;
 
-        double x = (two.getLongPos() - one.getLongPos()) * (Math.cos((one.getLatPos() + two.getLatPos())) / 2.0);
-        double y = two.getLatPos() - one.getLongPos();
+        double lat1 = one.getLatPos();
+        double lat2 = two.getLatPos();
+        double long1 = one.getLongPos();
+        double long2 = two.getLongPos();
 
-        distance = (double)Math.sqrt((double)(Math.pow(x,2)) + (double)Math.pow(y,2)) * 6371;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(long2 - long1);
+
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        distance = c * 6371;
+
+        System.out.println("Distance Between " + " long 1 " + one.getLongPos() + " lat 1 " + one.getLatPos()
+                            + " long 2 " + two.getLongPos() + " lat 2 " + two.getLatPos());
 
         return distance;
     }
+
+
 
     /**
      * Get closest position from default point
@@ -106,133 +129,11 @@ class Position {
     }
 
     public Position(String allData) {
+        String[] data;
+        allData = allData.replace(',','.');
+        data = allData.split(";");
 
         try {
-
-            allData = allData.replace(',','.');
-            String[] data = allData.split(";");
-
-            this.id = Integer.parseInt(data[0]);
-            this.name = data[1];
-            this.address = data[2];
-            this.phone = data[3];
-            this.longPos = Math.PI * Float.parseFloat(data[4]) / 180;
-            this.latPos = Math.PI * Float.parseFloat(data[5]) / 180.0;
-
-        } catch(Exception e) {
-            System.err.println(e + allData);
-        }
-    }
-
-
-}
-
-/*
-import java.util.*;
-import java.io.*;
-import java.math.*;
-
-
-class Solution {
-
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        String LON = in.next();
-        String LAT = in.next();
-
-
-        // User position :
-        Position defaultPos = new Position("0;default;here;nophone;" + LON + ";" + LAT);
-
-
-        // Number of items :
-        int N = in.nextInt();
-        if (in.hasNextLine()) {
-            in.nextLine();
-        }
-
-
-        String[] defibList = new String[N];
-        ArrayList<Position> posItems = new ArrayList<>();
-
-        // ex : id;name;address;phone;long;lat
-        for (int i = 0; i < N; i++) {
-            posItems.add(new Position(in.nextLine()));
-        }
-
-        Position reference = posItems.get(0);
-
-        // Get the closest here :
-        for (int i = 1; i < N; i++) {
-
-            reference = getClosest(defaultPos,reference,posItems.get(i));
-        }
-
-
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
-        System.out.println(reference.getName());
-    }
-
-
-    public static Float getDistance(Position one, Position two) {
-
-        Float distance;
-
-        Double x = (two.getLongPos() - one.getLongPos()) * (Math.cos((one.getLatPos() + two.getLatPos())) / 2.0);
-        Float y = two.getLatPos() - one.getLongPos();
-
-        distance = (float)Math.sqrt((float)(Math.pow(x,2)) + (float)Math.pow(y,2)) * 6371;
-
-        return distance;
-    }
-
-
-    public static Position getClosest(Position fromPos, Position a, Position b) {
-
-        return (getDistance(fromPos,a) < getDistance(fromPos,b))?a:b;
-    }
-}
-
-
-
-class Position {
-
-    private int id;
-    private String name;
-    private String address;
-    private String phone;
-    private float longPos;
-    private float latPos;
-
-    public float getLongPos() {
-        return longPos;
-    }
-
-    public void setLongPos(float longPos) {
-        this.longPos = longPos;
-    }
-
-    public float getLatPos() {
-        return latPos;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setLatPos(float latPos) {
-        this.latPos = latPos;
-    }
-
-    public Position(String allData) {
-
-        try {
-
-            allData = allData.replace(',','.');
-            String[] data = allData.split(";");
-
             this.id = Integer.parseInt(data[0]);
             this.name = data[1];
             this.address = data[2];
@@ -241,10 +142,9 @@ class Position {
             this.latPos = Float.parseFloat(data[5]);
 
         } catch(Exception e) {
-            System.err.println(e + allData);
+            System.err.println(e.getMessage() + "Size : " + data.length + " long : " + this.longPos + " lat : " + this.latPos);
         }
     }
 
 
 }
- */
