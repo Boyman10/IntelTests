f55e03dc18a4250ad556eeae847f0a9565db622c
Boyman10 <contact@yasi.fr>
2018-08-20 23:30:22 +0200

    Implementing bfs - workin on linkedlist for queue FIFO

--- src/graph/Bfs.java
+++ src/graph/Bfs.java
@@ -1,6 +1,9 @@
 package graph;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Breadth First Search algorihtm for a graph
@@ -16,13 +19,67 @@ public class Bfs {
      * Properties
      */
     private int V;   // No. of vertices
-    private LinkedList<Integer> adj[]; //Adjacency Lists
+    private List<List<Integer>> graph; //Adjacency Lists
 
     /**
      * Class constructor
-     *
+     * initialize tree
+     * ie: [[1, 2, 5], [0, 2, 5], [0, 1], [4, 5], [3, 5], [0, 1, 3, 4]]
      */
-    public Bfs() {
+    public Bfs(int size) {
+        V = size;
+        graph = new ArrayList<>();
+    }
 
+    /**
+     * Adding a node to the graph
+     * @param vertices
+     */
+    public void addVertice(List<Integer> vertices) {
+
+        graph.add(vertices);
+    }
+
+
+    /**
+     * Algorithm
+     * given source node, provide the different routes from it
+     */
+    public List<Integer> bfs(int sourceNode) {
+
+        // Mark all the vertices as not visited(By default
+        // set as false)
+        boolean visited[] = new boolean[V];
+
+        // Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        // Mark the current node as visited and enqueue it
+        visited[sourceNode]=true;
+        queue.add(sourceNode);
+
+        while (queue.size() != 0)
+        {
+            // Dequeue a vertex from queue and print it
+            sourceNode = queue.poll();
+
+            System.out.print(sourceNode + " "); // @TODO change to result type
+
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            Iterator<Integer> i = graph.get(sourceNode).listIterator();
+            while (i.hasNext())
+            {
+                int n = i.next();
+                if (!visited[n])
+                {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+
+        return null;
     }
 }
