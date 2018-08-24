23758b9fa22c0637b27e0d5a26261f472cf13494
Boyman10 <contact@yasi.fr>
2018-08-24 21:47:53 +0200

    Success working with hashtable - still going on with version 1 with array list - Unit testing

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -19,16 +19,18 @@ public class Graph {
     public static final int MIN_NODES = 2;
     public static final int MAX_LINKS = 1000;
     public static final int MIN_LINKS = 1;
-
+    public static final int MIN_EXITS = 1;
+    public static final int MAX_EXITS = 20;
 
     /**
      * Properties
      */
     private int V;   // No. of vertices
     private int L; // Nb of links
-    private List<List<Integer>> graphV1; //Adjacency Lists - ex : [[1, 2, 5], [0, 2, 5], [0, 1], [4, 5], [3, 5], [0, 1, 3, 4]]
 
-    private Hashtable<Object,List<Integer>> graphV2; // Another type of graph with keys which can be of any type - @Todo genericity
+    @Deprecated
+    private List<List<Integer>> graphV1; //Adjacency Lists - ex : [[1, 2, 5], [0, 2, 5], [0, 1], [4, 5], [3, 5], [0, 1, 3, 4]]
+    private Hashtable<Object,ArrayList<Integer>> graphV2; // Another type of graph with keys which can be of any type - @Todo genericity
 
 
     /**
@@ -39,11 +41,91 @@ public class Graph {
      */
     public Graph(int nbNodes, int nbLinks, int nbExits) {
 
-        graphV1 = new ArrayList<>();
-        V = nbNodes;
+        if (nbNodes >= MIN_NODES && nbNodes <= MAX_NODES && nbLinks <= MAX_LINKS
+                && nbLinks >= MIN_LINKS && nbExits >= MIN_EXITS && nbExits <= MAX_EXITS) {
 
+            graphV1 = new ArrayList<>();
+            graphV2 = new Hashtable<>();
+
+            V = nbNodes;
+
+        } else {
+            System.err.println("out of bounds...");
+        }
     }
 
 
+    /**
+     * Filling the graph one by one given the following
+     * @param srcNode source Node
+     * @param nextNode linked node
+     */
+    public void fillGraphV1(int srcNode, int nextNode) {
+
+        if (srcNode < V && srcNode >= 0 && nextNode < V && nextNode >= 0) {
+
+
+            ArrayList<Integer> nodes ;
+
+            try {
+                nodes =  (ArrayList<Integer>) graphV1.get(srcNode);
+            } catch (IndexOutOfBoundsException e) {
+                nodes = new ArrayList<>();
+            }
+
+            // Adding dest to the array
+            nodes.add(nextNode);
+            graphV1.add(srcNode, nodes);
+
+        } else {
+            System.err.println("OUt of bound Node!!");
+        }
+    }
+
+
+    /**
+     * Filling the graph one by one given the following
+     * @param srcNode source Node
+     * @param nextNode linked node
+     */
+    public void fillGraphV2(int srcNode, int nextNode) {
+
+        if (srcNode < V && srcNode >= 0 && nextNode < V && nextNode >= 0) {
+
+            ArrayList<Integer> nodes = (ArrayList<Integer>) graphV2.get(srcNode);
+            // Adding dest to the array
+            if (nodes == null) {
+                nodes = new ArrayList<>();
+            }
+
+            nodes.add(nextNode);
+            graphV2.put(srcNode, nodes);
+
+        } else {
+            System.err.println("OUt of bound Node!!");
+        }
+    }
+
+    /**
+     * REtrieve the generated graph
+     * @return
+     */
+    public Hashtable<Object,ArrayList<Integer>> getGraphV2() {
+
+        System.out.println(this.graphV2);
+
+        return graphV2;
+    }
+
+    /**
+     * REtrieve the generated graph
+     * @return
+     */
+    public List<List<Integer>> getGraphV1() {
+
+        System.out.println(this.graphV1);
+
+        return graphV1;
+    }
 
 }
--- test/graph/GraphTest.java
+++ test/graph/GraphTest.java
@@ -1,5 +1,12 @@
 package graph;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -7,5 +14,47 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class GraphTest {
 
+   private Graph graph;
+
+    @Test
+    void fillingGraphV1Test() {
+
+        //Example of graph to test :
+        graph = new Graph(4,4,1);
+
+        graph.fillGraphV1(0,1);
+        graph.fillGraphV1(0,2);
+        graph.fillGraphV1(1,3);
+        graph.fillGraphV1(2,3);
+
+        // the graph to obtain :
+        List<List<Integer>> graphV1 = new ArrayList<>();
+        graphV1.add(0,(new ArrayList(Arrays.asList(1,2))));
+        graphV1.add(1,(new ArrayList(Arrays.asList(3))));
+        graphV1.add(2,(new ArrayList(Arrays.asList(3))));
+
+        assertEquals(graphV1,graph.getGraphV1());
+
+    }
+    @Test
+    void fillingGraphV2Test() {
+
+        //Example of graph to test :
+        graph = new Graph(4,4,1);
+
+        graph.fillGraphV2(0,1);
+        graph.fillGraphV2(0,2);
+        graph.fillGraphV2(1,3);
+        graph.fillGraphV2(2,3);
+
+        // the graph to obtain :
+        Hashtable<Object,ArrayList<Integer>> graphV2 = new Hashtable<>();
+        graphV2.put(0,(new ArrayList(Arrays.asList(1,2))));
+        graphV2.put(1,(new ArrayList(Arrays.asList(3))));
+        graphV2.put(2,(new ArrayList(Arrays.asList(3))));
+
+        assertEquals(graphV2,graph.getGraphV2());
+
+    }
 
 }
