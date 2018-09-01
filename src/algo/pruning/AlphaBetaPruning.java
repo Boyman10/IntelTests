c3095b25ef8cfce06a23a3fbd4863d40e2817975
Boyman10 <contact@yasi.fr>
2018-09-01 23:23:23 +0200

    Preparing MinaMax and AlphaBeta pruning algorithm

---
+++ src/algo/pruning/AlphaBetaPruning.java
@@ -0,0 +1,9 @@
+package algo.pruning;
+
+/**
+ * Take the labeled graph and apply the algorithm
+ * a label represents an useful interval
+ * @see https://www.youtube.com/watch?v=KS2QkHe-hpE
+ */
+public class AlphaBetaPruning {
+}
---
+++ src/algo/pruning/MiniMax.java
@@ -0,0 +1,9 @@
+package algo.pruning;
+
+/**
+ * This class update a graph or tree to add labels to nodes so it can be treated in another algorithm such as AlphaBeta Pruning
+ * @see https://fr.wikipedia.org/wiki/%C3%89lagage_alpha-b%C3%AAta
+ * @eee https://www.youtube.com/watch?v=QTw8VJzRW6g&t=0s&list=PL6Xpj9I5qXYGhsvMWM53ZLfwUInzvYWsm&index=18
+ */
+public class MiniMax {
+}
