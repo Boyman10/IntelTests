0f85b40166299b8c031260d172ff5666ec093d2b
Boyman10 <contact@yasi.fr>
2018-09-07 21:09:33 +0200

    Preparing A* implementation on a puzzle.

---
+++ src/algo/astar/Labyrinth.java
@@ -0,0 +1,8 @@
+package algo.astar;
+
+/**
+ * Implementing BFS, A* for a specific labyrinth 2D game
+ * @see https://www.youtube.com/watch?v=8_dGurEKXDk&t=0s&list=PL6Xpj9I5qXYGhsvMWM53ZLfwUInzvYWsm&index=9
+ */
+public class Labyrinth {
+}
